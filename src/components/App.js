a1f90b6d4e6795d7624fdd67afa362b107bf37e7
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-21 20:27:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,37 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 25, conditions: "Sunny" });
+  const threshold = 20;
+
+  const handleTemperatureChange = (event) => {
+    setWeather({ ...weather, temperature: event.target.value });
+  };
+
+  const handleConditionsChange = (event) => {
+    setWeather({ ...weather, conditions: event.target.value });
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <h1>Weather App</h1>
+      <div className="temperature" style={{ color: weather.temperature > threshold ? "red" : "blue" }}>
+        {weather.temperature}°C
+      </div>
+      <div className="conditions">{weather.conditions}</div>
+      <form>
+        <label>
+          Temperature:
+          <input type="number" value={weather.temperature} onChange={handleTemperatureChange} />
+        </label>
+        <br />
+        <label>
+          Conditions:
+          <input type="text" value={weather.conditions} onChange={handleConditionsChange} />
+        </label>
+      </form>
     </div>
-  )
+  );
 }
 
 export default App
